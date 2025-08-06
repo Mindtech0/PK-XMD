2c00c8b2c524858e1c497cb4699bf53e24a325f4
Mindtech0 <earnonline4k@gmail.com>
2025-08-06 17:30:38 +0300

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "pk~KhIgDQzT#49L0RKNSz3cLdIcQk8AFkMKObfU1KA-BLIhxx9ww3h0",
+SESSION_ID: process.env.SESSION_ID || "bbvdpk~KhIgDQzT#49L0RKNSz3cLdIcQk8AFkMKObfU1KA-BLIhxx9ww3h0",
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
 AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || "false",
 AUTO_STATUS_REACT: process.env.AUTO_STATUS_REACT || "true",
